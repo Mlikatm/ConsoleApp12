febfb47acee3855c5670db2256007ab553e0984e
melika <melikatorabi031@gmail.com>
2023-04-15 21:46:03 +0430

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace project
+{
+    class test1
+    {
+        public static void Main()
+        {
+            int X= Convert.ToInt32(Console.ReadLine());
+            int Y= Convert.ToInt32(Console.ReadLine());
+            calc (X,Y);
+            private void calc (int x, int y)
+            {
+                double Result= Math.Sqrt(Math.Abs(x) + Math.Pow (y,2));
+                Console.WriteLine(Result);
+
+
+            }
+
+        }//end function
+    }//end class
+}//end namespace
